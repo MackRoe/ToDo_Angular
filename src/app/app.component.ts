e11b67a1b40cf320a420ad7661f0a1ad39dd7644
MackRoe <elaine.music@students.makeschool.com>
2021-01-09 21:19:14 -0500

    input-button-unit component displays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
     <h1>
       Welcome to {{ title }}, your personal custom made To-Do List!
     </h1>
+
+    <app-input-button-unit></app-input-button-unit>
   `,
   styleUrls: ['./app.component.scss']
 })
