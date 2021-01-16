f21c28a5e093fc5e87cf64dbd4b3a7f2b9483789
MackRoe <elaine.music@students.makeschool.com>
2021-01-16 09:31:06 -0500

    iteratively displays list items

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -4,13 +4,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
 
-    <input [value]='title'>
-    <button (click)="changeTitle('Button Clicked!')">
+    <input #inputElementRef
+      [value]='title'
+      (keyup.enter)="changeTitle($event.target.value)">
+    <button (click)="changeTitle(inputElementRef.value)">
       Save
     </button>
   `,
